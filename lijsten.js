26cb64cddc1137a0e0b4af171a39925a327f2349
Steven De Ryck <steven@inform.be>
2020-03-03 12:55:10 +0100

    Initial commit

---
+++ lijsten.js
@@ -0,0 +1,22 @@
+var personen = [
+    { id:1, voornaam: 'Alexander',naam:'Van Den Broeck',adres: 'Oude baan 1'},
+    { id:2, voornaam: 'Anneleen ',naam:'Peeters',adres: 'Oude baan 2'},
+    { id:3, voornaam: 'Arno',naam:'Bosmans',adres: 'Oude baan 3'},
+    { id:4, voornaam: 'Daan',naam:'Luyten',adres: 'Oude baan 4'},
+    { id:5, voornaam: 'Gentian',naam:'Collaku',adres: 'Oude baan 5'},
+    { id:6, voornaam: 'Hannelore',naam:'Lamotte',adres: 'Oude baan 6'},
+    { id:7, voornaam: 'Jeremy',naam:'Gigase',adres: 'Oude baan 7'},
+    { id:8, voornaam: 'Joannes',naam:'Dries',adres: 'Oude baan 8'},
+    { id:9, voornaam: 'Jordi',naam:'Maes',adres: 'Oude baan 9'},
+    { id:10, voornaam: 'Laurens',naam:'Vandenbergh',adres: 'Oude baan 10'},
+    { id:11, voornaam: 'Maya',naam:'Vekemans',adres: 'Oude baan 11'},
+    { id:12, voornaam: 'Nadir',naam:'Hadnan',adres: 'Oude baan 12'},
+    { id:13, voornaam: 'Nathan',naam:'Ziarczyk',adres: 'Oude baan 13'},
+    { id:14, voornaam: 'Nicole',naam:'Furtado',adres: 'Oude baan 14'},
+    { id:15, voornaam: 'Rob',naam:'Kwanten',adres: 'Oude baan 15'},
+    { id:16, voornaam: 'Roel',naam:'Van Bilzen',adres: 'Oude baan 15'},
+    { id:17, voornaam: 'Wannes',naam:'Vrijs',adres: 'Oude baan 15'},
+];
+
+var vakken = ['Internet', 'Databases', 'Frontend', 'Backend'];
+
